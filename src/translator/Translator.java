31cbeced8cba9b146464fb588d729f5f715fd0d9
Ondřej Němec <nikdynikde@gmail.com>
2018-07-18 23:34:48 +0200

    Added translation with count

--- src/translator/Translator.java
+++ src/translator/Translator.java
@@ -13,7 +13,12 @@ public class Translator {
 	
 	private char endSeparator = '%';
 	
+	private String messageSeparator = ";";
+	
+	private String countSeparator = "~";
+	
 	private final ResourceBundle messages;
+	
 	
 	public Translator(String path) {
 		this.messages = ResourceBundle.getBundle(path);
@@ -34,6 +39,10 @@ public class Translator {
 	
 	public String translate(String key, String... variables) {
 		String message = translate(key);
+		return translateVariables(message, variables);
+	}
+	
+	private String translateVariables(String message, String... variables) {
 		Pattern patern = Pattern.compile("(" + startSeparator + "[^ ]*" + endSeparator + ")");
 		Matcher matcher = patern.matcher(message);
 		int i = 0;
@@ -44,6 +53,56 @@ public class Translator {
 			i++;
 		}
 		return message;
+	}
+	
+	/*
+	 * n
+	 * -n
+	 * n-m
+	 * -n--m
+	 * n<
+	 * n<=
+	 * n>
+	 * n>=
+	 */
+	public String translate(String key, int count){
+		String[] messages = translate(key).split(messageSeparator);
+		for (String mess : messages) {
+			String[] splited = mess.split(countSeparator);
+			for (String stringCount : splited[0].split(",")){
+				// n OR -n
+				Pattern p = Pattern.compile("^-?[0-9]+$");
+				Matcher m = p.matcher(stringCount);
+				if(m.find() && Integer.parseInt(m.group()) == count)
+					return translateVariables(splited[1], new Integer(count).toString());
+				// n-m OR -n-m OR -n-m
+				p = Pattern.compile("^(-?[0-9]+)(-)(-?[0-9]+)$");
+				m = p.matcher(stringCount);
+				if(m.find() && Integer.parseInt(m.group(1)) <= count && count <= Integer.parseInt(m.group(3))){
+					return translateVariables(splited[1], new Integer(count).toString());
+				}
+				// n< OR n<= OR n> OR n>= 
+				p = Pattern.compile("^([0-9]+)(<|<=|>|>=)$");
+				m = p.matcher(stringCount);
+				if(m.find()){
+					switch(m.group(2)){
+					case "<":
+						if(new Integer(m.group(1)) < count)
+							return translateVariables(splited[1], new Integer(count).toString());
+					case "<=":
+						if(new Integer(m.group(1)) <= count)
+							return translateVariables(splited[1], new Integer(count).toString());
+					case ">":
+						if(new Integer(m.group(1)) > count)
+							return translateVariables(splited[1], new Integer(count).toString());
+					case ">=":
+						if(new Integer(m.group(1)) >= count)
+							return translateVariables(splited[1], new Integer(count).toString());
+					}
+				}
+			}			
+		}
+		return key + " : " + count;
 	}
 	
 	public void setVariableSeparators(char start, char end) {
@@ -66,4 +125,21 @@ public class Translator {
 	public boolean isDebug() {
 		return debug;
 	}
+	
+	public void setMessageSeparator(String separator){
+		this.messageSeparator = separator;
+	}
+	
+	public String getMessageSeparator(){
+		return messageSeparator;
+	}
+	
+	public void setCountSeparator(String separator){
+		this.countSeparator = separator;
+	}
+	
+	public String getCountSeparator(){
+		return countSeparator;
+	}
+
 }
--- tests/translator/TranslatorTest.java
+++ tests/translator/TranslatorTest.java
@@ -13,50 +13,42 @@ import org.junit.runners.Parameterized.Parameters;
 @RunWith(Parameterized.class)
 public class TranslatorTest {
 
-	protected String key;
-	protected String message;
-	protected boolean debug;
-	protected String[] variables;
+	private String path = "translator/messages";
 	
-	public TranslatorTest(String key, String message, boolean debug, String[] variables) {
+	private String key;
+	private String message;
+	private boolean debug;
+	private int count;
+	
+	public TranslatorTest(String key, String message, boolean debug, int count) {
 		this.key = key;
 		this.message = message;
 		this.debug = debug;
-		this.variables = variables;
-	}
-	
-	@Test
-	public void testSetDebugWorks(){
-		Translator translator = new Translator("translator/messages");
-		assertFalse(translator.isDebug());
-		
-		translator.setDegug(true);
-		assertTrue(translator.isDebug());
-		
-		translator.setDegug(false);
-		assertFalse(translator.isDebug());
-	}
-		
-	@Test
-	public void testTranslateWorks(){
-		Translator translator = new Translator("translator/messages");
-		translator.setDegug(debug);
-		assertEquals(message, translator.translate(key));
+		this.count = count;
 	}
 
 	@Test(expected = RuntimeException.class)
 	public void testTranslateWithVariablesThrowsWhenVariableDontExist(){
-		Translator translator = new Translator("translator/messages");
-		translator.translate("too %many% %variables%", "var");
+		Translator translator = new Translator(path);
+		translator.translate("too.many.variables", "var");
 	}
-	
+			
+	@Test
+	public void testTranslateWorks(){
+		if(key.equals("test.count"))
+			return;
+		Translator translator = new Translator(path);
+		translator.setDegug(debug);
+		assertEquals(message, translator.translate(key));
+	}
+
 	@Test
 	public void testTranslateWithVariablesWorks(){
-		Translator translator = new Translator("translator/messages");
+		Translator translator = new Translator(path);
 		assertEquals(
 				"You choose: 4 pieces, 7 liters",
 				translator.translate(
-						"test.choose%pieces%%liters%",
+						"test.choose.pieces.liters.%%",
 						new Integer(4).toString(),
 						new Integer(7).toString())
 			);
@@ -64,19 +56,39 @@ public class TranslatorTest {
 		assertEquals(
 				"You choose: 8 pieces, 45 liters",
 				translator.translate(
-						"test.choose<pieces><liters>",
+						"test.choose.pieces.liters.<>",
 						new Integer(8).toString(),
 						new Integer(45).toString())
+			);
+	}
+	
+	@Test
+	public void testTranslateWithCountWorks(){
+		if(!key.equals("test.count"))
+			return;
+		Translator translator = new Translator(path);
+		assertEquals(
+				message,
+				translator.translate(key, count)
 			);
 	}
 		
 	@Parameters
 	public static Collection<Object[]> dataSet(){
 		return Arrays.asList(
-					new Object[]{"test.success", "Successful test", false, null},
-					new Object[]{"test.success", "Successful test", true, null},
-					new Object[]{"test.failure", "test.failure", false, null},
-					new Object[]{"test.failure", "??? test.failure !!!", true, null}
+					new Object[]{"test.success", "Successful test", false, 0},
+					new Object[]{"test.success", "Successful test", true, 0},
+					new Object[]{"test.failure", "test.failure", false, 0},
+					new Object[]{"test.failure", "??? test.failure !!!", true, 0},
+					
+					
+					new Object[]{"test.count", "No years", false, 0},
+					new Object[]{"test.count", "1 year", false, 1},
+					new Object[]{"test.count", "years: 3", false, 3},
+					new Object[]{"test.count", "comma, year-5", false, 5},
+					new Object[]{"test.count", "comma, year-6", false, 6},
+					new Object[]{"test.count", "more: 9", false, 9},
+					new Object[]{"test.count", "less year", false, -1}
 				);
 	}
 	
